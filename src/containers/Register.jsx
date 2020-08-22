358a29aeea0169f25f2787c9f27a00881bd3118d
Matias Gallegos <gallegosmatiasjoel@gmail.com>
2020-08-21 22:33:45 -0300

    Register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,38 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 import g from '../assets/static/g.png';
 import tw from '../assets/static/tw.png';
 
-const Register = () => (
-  <section className="login">
-    <div className="login__container">
-      <h2 tabindex="0">Registrarse</h2>
-      <form action="" className="login__container--form">
-      <input aria-label="name" type="text" className="input" placeholder="Nombre"/>
-      <input aria-label="email" type="text" className="input" placeholder="Email"/>
-        <input aria-label="password"  type="password" className="input" placeholder="Contraseña"/>
-        <button className="button">Registrarse</button>
-        <div className="login__container--remember-me">
-          <label>
-            <input type="checkbox" id="cbox1" value="checkbox"/>Recordarme
-          </label>
-        </div>
-      </form>
+const Register = () => {
+    const [form, setValues] = useState({
+      email: '',
+      name: '',
+      password: '',
+    });
 
-      <section className="login__container--social-media">
-        <div><img src={g} alt="Google"/>Registrarse con Google</div>
-        <div><img src={tw} alt="Twitter"/>Registrarse con Twitter</div>
+    const handleInput = event => {
+      setValues({
+        ...form,
+        [event.target.name]: event.target.value
+      })
+    };
+
+    const handleSubmit = event => {
+      event.preventDefault();
+      console.log(form);
+    };
+
+    return (
+      <section className="login">
+        <div className="login__container">
+          <h2 tabindex="0">Registrarse</h2>
+          <form action="" className="login__container--form" onSubmit={handleSubmit}>
+
+          <input
+            name="name"
+            className="input"
+            aria-label="name"
+            type="text"
+            placeholder="Nombre"
+            onChange={handleInput}
+          />
+
+          <input
+            name="email"
+            className="input"
+            aria-label="email"
+            type="text"
+            placeholder="Email"
+            onChange={handleInput}
+          />
+
+          <input
+            name="password"
+            className="input"
+            aria-label="password"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+
+          <button className="button" type="submit">Registrarse</button>
+            <div className="login__container--remember-me">
+              <label>
+                <input type="checkbox" id="cbox1" value="checkbox"/>Recordarme
+              </label>
+            </div>
+          </form>
+
+          <section className="login__container--social-media">
+            <div><img src={g} alt="Google"/>Registrarse con Google</div>
+            <div><img src={tw} alt="Twitter"/>Registrarse con Twitter</div>
+          </section>
+
+          <p className="login__container--register">¿Ya tenés una cuenta?
+            <Link to="/login">
+              Iniciar sesión
+            </Link>
+          </p>
+        </div>
       </section>
 
-      <p className="login__container--register">¿Ya tenés una cuenta? 
-        <Link to="/login">
-          Iniciar sesión
-        </Link>
-      </p>
-    </div>
-  </section>
-
-);
+  );
+};
 
 export default Register;
